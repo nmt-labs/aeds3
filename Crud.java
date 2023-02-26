05a6810b602e56dd076f16f0e2e50e0315b7a4eb
Nathalia Tenaglia <nathtenaglia@hotmail.com>
2023-02-26 18:13:14 -0300

    crud delete

--- Crud.java
+++ Crud.java
@@ -1,62 +1,87 @@
 import java.io.File;
-import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Crud {
 
-    private String nomeDoArquivo = "musicas.db";
-    private RandomAccessFile arquivo;
+  private String nomeDoArquivo = "musicas.db";
+  private RandomAccessFile arquivo;
 
-    public Crud() {
-        boolean verificaArquivo = (new File(nomeDoArquivo)).exists();
-        if (!verificaArquivo) { // talvez criar o arquivo caso ele nao exista
-            System.out.print("Arquivo não existe!");
-        }
+  public Crud() {
+      boolean verificaArquivo = (new File(nomeDoArquivo)).exists();
+      if (!verificaArquivo) { // talvez criar o arquivo caso ele nao exista
+          System.out.print("Arquivo não existe!");
+      }
+  }
+
+  // Método de inserção no arquivo
+  public void create(Musica musica) throws Exception {
+    arquivo = new RandomAccessFile(nomeDoArquivo, "rw");
+
+    byte[] ba = musica.toByteArray(); // converte musica para byte
+    arquivo.seek(0); // move ponteiro para o inicio do arquivo
+    arquivo.writeInt(musica.getId()); // escreve id da ultima muisca no inicio do arquivo
+    arquivo.seek(arquivo.length());// mover para o fim do arquivo
+
+    // escrever registro
+    arquivo.writeByte(' '); // escreve a lápide
+    arquivo.writeInt(ba.length); // escreve tamanho do registro
+    arquivo.write(ba);
+
+    arquivo.close();
+    System.out.println("Música adicionada com sucesso! Seu id é " + musica.getId());
+  }
+
+  // Método de leitura do arquivo
+  public Musica read(int id) throws Exception {
+    arquivo = new RandomAccessFile(nomeDoArquivo, "rw");
+    byte[] ba;
+    int tamanho = arquivo.readInt();
+    Byte lapide = arquivo.readByte();
+    Musica musica = new Musica();
+
+    arquivo.seek(4); // move ponteiro para o primeiro registro
+    while (arquivo.getFilePointer() != arquivo.length()) {
+      arquivo.seek(arquivo.getFilePointer() + 1);
+      ba = new byte[tamanho];
+      arquivo.read(ba);
+      if (lapide != '*') {
+        musica.fromByteArray(ba);
+        if (musica.getId() == id)
+          return musica;
+      }
     }
 
-    // Método de inserção no arquivo
-    public void create(Musica musica) throws Exception {
-        try {
-            arquivo = new RandomAccessFile(nomeDoArquivo, "rw");
+    arquivo.close(); 
+    return null;
+  }
 
-            byte[] ba = musica.toByteArray(); // converte musica para byte
-            arquivo.seek(0); // move ponteiro para o inicio do arquivo
-            arquivo.writeInt(musica.getId()); // escreve id da ultima muisca no inicio do arquivo
-            arquivo.seek(arquivo.length());// mover para o fim do arquivo
+  // Método de exclusão do arquivo
+  public Musica delete(int id) throws Exception {
+    arquivo = new RandomAccessFile(nomeDoArquivo, "rw");
+    byte[] ba;
+    int tamanho;
+    Byte lapide = arquivo.readByte();
+    long posicao;
+    Musica musica = new Musica();
 
-            // escrever registro
-            arquivo.writeByte(' '); // escreve a lápide
-            arquivo.writeInt(ba.length); // escreve tamanho do registro
-            arquivo.write(ba);
-
-            arquivo.close();
-
-            System.out.println("Música adicionada com sucesso! Seu id é " + musica.getId());
-        } catch (IOException e) {
-            System.out.println("Erro ao adicionar música!");
-        }
-
+    arquivo.seek(4); // move ponteiro para o primeiro registro
+    while (arquivo.getFilePointer() != arquivo.length()) {
+      posicao = arquivo.getFilePointer(); // posicao atual do ponteiro no arquivo
+      tamanho = arquivo.readInt();
+      ba = new byte[tamanho];
+      arquivo.read(ba);
+      if (lapide != '*') {
+        musica.fromByteArray(ba);
+        if (musica.getId() == id)
+        arquivo.seek(posicao); // volta para a posicao inicial do registro
+        arquivo.writeChar('*'); // marca a lapide
+        arquivo.close();
+        return musica;
+      }
     }
 
-    // Método de leitura do arquivo
-    public Musica read(int id) throws Exception {
-        arquivo = new RandomAccessFile(nomeDoArquivo, "rw");
-        byte[] ba;
-        short tamanho = arquivo.readShort();
-        Musica musica = new Musica();
-
-        arquivo.seek(4); // move ponteiro para o primeiro registro
-        while (arquivo.getFilePointer() != arquivo.length()) {
-            arquivo.seek(arquivo.getFilePointer() + 1);
-            ba = new byte[tamanho];
-            arquivo.read(ba);
-            if (arquivo.readByte() != '*') {
-                musica.fromByteArray(ba);
-                if (musica.getId() == id)
-                    return musica;
-            }
-        }
-        return null;
-    }
+    arquivo.close();
+    return null;
+  }
 
 }
