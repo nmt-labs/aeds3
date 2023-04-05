26b2fbbb05d710e1475fa9afc480505f4f7074c9
Nathalia Tenaglia <nathtenaglia@hotmail.com>
2023-04-05 17:22:20 -0300

    b plus tree

---
+++ bplustree/Key.java
@@ -0,0 +1,11 @@
+package bplustree;
+
+public class Key {
+  public int id;
+  public long pointer;
+
+  public Key(int id, long pointer) {
+    this.id = id;
+    this.pointer = pointer;
+  }
+}
---
+++ bplustree/No.java
@@ -0,0 +1,44 @@
+package bplustree;
+
+public class No {
+  public int ordem;
+  public Key[] chaves;
+  public No[] filhos;
+  public int nChaves;
+  public boolean folha;
+  public No irmao;
+
+  public No(int ordem) {
+    this.ordem = ordem;
+    this.chaves = new Key[ordem - 1];
+    this.filhos = new No[ordem];
+    this.nChaves = 0;
+    this.folha = true;
+    this.irmao = null;
+  }
+
+  public void print() {
+    System.out.print("[");
+    for (int i = 0; i < nChaves; i++) {
+      System.out.print(chaves[i] == null ? "null" : chaves[i].id);
+      if (i < nChaves - 1) {
+        System.out.print(", ");
+      }
+    }
+    System.out.print("]");
+  }
+
+  public No clone() {
+    No novo = new No(ordem);
+    novo.nChaves = nChaves;
+    novo.folha = folha;
+    novo.irmao = irmao;
+    for (int i = 0; i < nChaves; i++) {
+      novo.chaves[i] = chaves[i];
+    }
+    for (int i = 0; i <= nChaves; i++) {
+      novo.filhos[i] = filhos[i];
+    }
+    return novo;
+  }
+}
