85703f75f3f3fedb1d38b41e0a9c455a10f5b85c
Nathália Tenaglia <101769137+inathlia@users.noreply.github.com>
2023-04-10 17:08:22 -0300

    Update Key.java

--- bplustree/Key.java
+++ bplustree/Key.java
@@ -1,11 +1,53 @@
 package bplustree;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+
 public class Key {
-  public int id;
-  public long pointer;
+  private int id;
+  private long pointer;
+
+  public Key() {
+    this.id = -1;
+    this.pointer = -1;
+  }
 
   public Key(int id, long pointer) {
     this.id = id;
     this.pointer = pointer;
+  }
+
+  public int getId() {return id;}
+  public long getPointer() {return pointer;}
+  public void setId(int id) {this.id = id;}
+  public void setPointer(long pointer) {this.pointer = pointer;}
+
+  public byte[] toByteArray() throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+
+    dos.writeInt(this.id);
+    dos.writeLong(this.pointer);
+
+    return baos.toByteArray();
+  }
+
+  public void fromByteArray(byte[] ba) throws IOException, ParseException {
+    ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+    DataInputStream dis = new DataInputStream(bais);
+
+    this.id = dis.readInt();
+    this.pointer = dis.readLong();
+  }
+
+  public String toString(){
+    
+    return 
+    "\nId: " + this.id +
+    "\nPointer: " + this.pointer;
   }
 }
